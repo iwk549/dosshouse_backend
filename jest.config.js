e1297629521ea6b70a1895739ac955f28fd0b69e
Ian Kendall <=>
2025-06-17 14:37:34 -0700

    30: implement test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   globalTeardown: "<rootDir>/test-teardown-globals.js",
+  testEnvironment: "node",
+  coverageThreshold: {
+    // these are set to ensure no regression in coverage
+    // update these as coverage improves
+    global: {
+      statements: 90,
+      branches: 80,
+      functions: 85,
+      lines: 90,
+    },
+  },
+  coverageReporters: ["text", "lcov", "json-summary"],
 };
